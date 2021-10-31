62e3bae8458d68ee44bc8c4ddd72af6cf982167a
ljm12914 <LJM12914@outlook.com>
2021-10-31 13:53:23 +0800

    0.5.1.6
    z-index基本重写完毕；tooltip进行中

--- mwds.js
+++ mwds.js
@@ -29,9 +29,9 @@ $(function(){
 //可关闭的窗口
     clsAddToolTip();//给ds-cls添加提示框
     //鼠标
-    $(".ds-cls").on("dblclick",function(e){if($(e.target).hasClass("ds-cls")) $(e.target).hide();});//防止事件冒泡
+    $(".ds-cls").on("dblclick",function(e){closeCls(false,e)});
     //触摸屏（oink：仅限触摸屏电脑,移动端会直接上菜单）
-    $(".ds-cls").on("touchstart",function(e){if($(e.target).hasClass("ds-cls")) checkClose(t);});//FIXME:这里由于子节点的touchstart传不上来，暂时无法在点击窗口内容时出现提示。
+    $(".ds-cls").on("touchstart",function(e){closeCls(true,e);});//FIXME:这里由于子节点的touchstart传不上来，无法在点击窗口内容时出现提示。
 //end可关闭的窗口
 //overlay创建
     createMask();//遮罩创建
@@ -46,7 +46,7 @@ $(function(){
 
 //移动方法
     //general按下处理
-function pressDown(isTouch,e){
+function pressDown(isTouch, e){
     let t = e.target;
     while(true){//如果在窗口内发生则提升z-index
         if($(t).hasClass("ds-win")){
@@ -60,10 +60,11 @@ function pressDown(isTouch,e){
     }
 }
     //ds-win按下处理
-function dsWinPress(isTouch,e){
+function dsWinPress(isTouch, e){
     moving = e.target;
     movingj = $(moving);
     if(movingj.hasClass("ds-mov")){//防止事件冒泡
+        movingj.css("z-index","1000");
         ismoving = true;
         if(isTouch){
             deltatop = e.touches[0].pageY - movingj.position().top;
@@ -77,11 +78,10 @@ function dsWinPress(isTouch,e){
     }
 }
     //移动中处理+边界探测
-function movingf(isTouch,e){
+function movingf(isTouch, e){
     if(ismoving){
         //zIndex($(e));
         if(isTouch){
-            zIndex(movingj);
             movingj.css("top",e.touches[0].pageY - deltatop);
             movingj.css("left",e.touches[0].pageX - deltaleft);
         }
@@ -99,6 +99,7 @@ function movingf(isTouch,e){
 function moveUp(){
     if(ismoving){
         ismoving = false;
+        setZ(movingj,false,50);
         zIndex(movingj);//重新计算z-index
         delStyle($("html")[0]);
     }
@@ -115,7 +116,7 @@ function delStyle(dfs){
 
 //动态提升方法
     //判断a在b上面还是下面+检测
-function tOrb(a,b){
+function tOrb(a, b){
     a = $(a);
     b = $(b);
     let t = a.offset().top;
@@ -127,32 +128,33 @@ function tOrb(a,b){
     let l1 = b.offset().left;
     let r1 = l1 + b.innerWidth();
     if(((t>t1&&t<o1)||(o>t1&&o<o1)||(t1>t&&t1<o)||(o1>t&&o1<o))&&((l>l1&&l<r1)||(r>l1&&r<r1)||(l1>l&&l1<r)||(r1>l&&r1<r))){//判断是否覆盖
-        if(parseInt($(a).css("z-index")) > parseInt($(b).css("z-index"))) return "a";//a在上
-        else if(parseInt($(a).css("z-index")) < parseInt($(b).css("z-index"))) return "b";//a在下
+        if(getZ(a) > getZ(b)) return "a";//a在上
+        else if(getZ(a) < getZ(b)) return "b";//a在下
         else return "s";//两个一样
     }else return "e";//根本就没覆盖
 }
+    //快速获取z-index
+function getZ(obj){return parseInt($(obj).css("z-index"));}
+    //递增/修改z-index
+function setZ(obj, isPlus, p){//p可以是负数！
+    if(p && isPlus) $(obj).css("z-index",getZ(obj) + p);
+    else if(isPlus) $(obj).css("z-index",getZ(obj) + 1);
+    else $(obj).css("z-index",p);
+    return getZ(obj);
+}
 
+    //dark名鼎鼎的zIndex方法
 function zIndex(obj){
-    //TODO:zindex
-    let coverlist = [];
+    //FIXME:zindex基本能用了。赶快fix掉这个bug/斜眼笑
+    setZ(obj,false,50);
     for(let i = 0; i < winlist.length; i++){
         let torb = tOrb(obj,$(winlist[i]));
-        if(torb == "b" || torb == "s" && !$(winlist[i]).hasClass("ds-zin")){
-            coverlist[coverlist.length] = $(winlist[i]);//FIXME:为啥这里™开始玄学了啊？为什么？！
+        if(torb != "e" && !$(winlist[i]).hasClass("ds-zin") && $(winlist[i]).css("display") != "none"){
+            obj.addClass("ds-zin");
+            zIndex($(winlist[i]));
+            setZ(obj,false,getZ($(winlist[i])) + 1);
+            obj.removeClass("ds-zin");
         }
-    }
-    if(coverlist.length != 0){//减少复杂度
-        obj.css("z-index",50 + coverlist.length);
-        obj.addClass("ds-zin");
-        console.log(obj[0].classList);
-        for(let i = 0; i < coverlist.length; i++){
-            zIndex(coverlist[i]);
-        }
-        obj.removeClass("ds-zin");
-    }
-    else{
-        return;//强行回去还不行吗？！
     }
 }
 //end动态提升方法
@@ -161,20 +163,39 @@ function zIndex(obj){
 function clsAddToolTip(){
     let c = document.createElement("span");
     $(c).addClass("ds-tooltip ds-tgra");
-    c.innerText = "双击关闭窗口";
+    c.innerText = "在空闲区域双击可关闭窗口";
     $(".ds-cls").prepend(c);
 }
-function checkClose(){
-
+function closeCls(isTouch, e){
+    let t = $(e.target);
+    if(!t.hasClass("ds-cls")) return;
+    if(isTouch){
+        //setTimeout()//TODO:触摸屏端检测双击
+    }
+    else{
+        setZ(t,false,50);
+        t.hide();
+    }
 }
 //endds-cls相关
 
 //提示框
 function alignToolTip(e){
-    e = $(e.target);
-    let tip = e.children(".ds-tooltip");
+    obj = $(e.target);
+    let tip = obj.children(".ds-tooltip");
     for(let i = 0; i < tip.length; i++){
-        $(tip[i]).css("left",$(e).innerWidth() / 2 - $(tip[i]).outerWidth() / 2 + "px");
+        $(tip[i]).css("top",obj + "px");//先在顶部渲染
+        $(tip[i]).css("left",obj.innerWidth() / 2 - $(tip[i]).outerWidth() / 2 + "px");
+        if($(tip[i]).hasClass("ds-showb")){
+
+        }
+        else if($(tip[i]).hasClass("ds-showl")){
+
+        }
+        else if($(tip[i]).hasClass("ds-showr")){
+
+        }
+        
     }
 }
 //end提示框
